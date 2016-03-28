bafb9f299e27873dd2cf9f351135f80ecd83e24c
Kbij <Kbij@users.noreply.github.com>
2016-03-28 11:24:47 +0200

    test working

--- Server/HomeControlTest/src/MainTest.cpp
+++ Server/HomeControlTest/src/MainTest.cpp
@@ -5,6 +5,23 @@
  *      Author: koen
  */
 
+#include "gtest/gtest.h"
+#include <glog/logging.h>
+#include <gflags/gflags.h>
 
+GTEST_API_ int main(int argc, char **argv)
+{
+	google::InitGoogleLogging("Home Contron Test");
+	testing::InitGoogleTest(&argc, argv);
+//	testing::GTEST_FLAG(filter) = "SocketComm.*";
 
+	FLAGS_logtostderr=1;
+	FLAGS_v = 3;
 
+	//FLAGS_minloglevel = 100;
+    int result = RUN_ALL_TESTS();
+
+	google::ShutdownGoogleLogging();
+	google::ShutDownCommandLineFlags();
+	return result;
+}
