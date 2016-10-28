03dc86956793f2c57e2060ab3364608793008926
Kbij <Kbij@users.noreply.github.com>
2016-10-28 18:57:15 +0200

    cleanup + software serial debug

--- Arduino/Sensor/Sensor.ino
+++ Arduino/Sensor/Sensor.ino
@@ -1,5 +1,5 @@
-
-//#include <Printers.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <CapacitiveSensor.h>
@@ -14,17 +14,26 @@ const int TOUCH_COMMON = 3;
 const int TOUCH_UP = 4;
 const int TOUCH_DOWN = 5;
 const int TRANSMIT_LED = 6;
+const int OLED_ADDRESS = 0x3C;
+const int TEMP_INTERVAL_SECONDS = 30;
+const int TEMP_MEASURE_TIME_SECONDS = 1;
 
 // DS18S20 Setup, use pin 2
 OneWire oneWire(ONEWIRE_PIN);
 DallasTemperature sensors(&oneWire);
 
+//Software serial (debug)
+SoftwareSerial debugSerial(8, 9); // RX, TX
+
 // Touch buttons
 CapacitiveSensor touchUp   = CapacitiveSensor(TOUCH_COMMON, TOUCH_UP);
 CapacitiveSensor touchDown = CapacitiveSensor(TOUCH_COMMON, TOUCH_DOWN);
 
-const int TEMP_INTERVAL_SECONDS = 30;
-const int TEMP_MEASURE_TIME_SECONDS = 1;
+
+// create the XBee object
+XBee xbee = XBee();
+
+
 
 char ADDR_STR[17];
 char SET_TEMPERATURE[10];
@@ -38,9 +47,7 @@ bool bootFlag = true;
 bool displaySetTemperature = false;
 bool setTempReceived = true;
 bool lastAcknowledged = false;
-
-// create the XBee object
-XBee xbee = XBee();
+bool oledPresent = false;
 
 //At startup we are broadcasting
 XBeeAddress64 sensorListener = XBeeAddress64(0x00000000, 0x0000FFFF);
@@ -54,6 +61,12 @@ uint8_t idCmd[] = {'I','D'};
 uint8_t idValue[] = {0x12, 0x13};
 
 AtCommandRequest atRequest = AtCommandRequest(chCmd);
+void blinkLed()
+{
+  digitalWrite(TRANSMIT_LED, LOW);
+  delay(1000); 
+  digitalWrite(TRANSMIT_LED, HIGH);
+}
 
 void xbeeSend(const char *format, ...)
 {
@@ -65,6 +78,8 @@ void xbeeSend(const char *format, ...)
 
   ZBTxRequest zbString = ZBTxRequest(sensorListener, (uint8_t*)buf, strlen(buf));
 
+  debugSerial.print("Xbee send:"); 
+  debugSerial.println(buf); 
   digitalWrite(TRANSMIT_LED, LOW);
   xbee.send(zbString);
   digitalWrite(TRANSMIT_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
@@ -110,7 +125,7 @@ void draw()
     u8g.drawStr( 118, 15, "!");  
   } else
   {
- //   u8g.drawStr( 118, 15, " ");
+    u8g.drawStr( 118, 15, " ");
   }
 
   u8g.drawFrame(0, 0, 128, 64);
@@ -126,10 +141,16 @@ void sendAtCommand()
 
 void setup()
 {
+  pinMode(TRANSMIT_LED, OUTPUT);
+  blinkLed();  
+
+  debugSerial.begin(9600);
+  debugSerial.println("Sensor Init");  
+  blinkLed();
+  
   // start serial port
   Serial.begin(38400);
   xbee.setSerial(Serial);
-  pinMode(TRANSMIT_LED, OUTPUT);
   
   touchUp.set_CS_AutocaL_Millis(1000);
   touchDown.set_CS_AutocaL_Millis(1000);
@@ -149,27 +170,43 @@ void setup()
   
   // Start up the library
   sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
+  
+  //Get the address if the first onewire device
   uint8_t sensorAddr[8];
-  sensors.getAddress(sensorAddr, 0);
+  sensors.getAddress(sensorAddr, 0); 
   memset(ADDR_STR, 0, sizeof(ADDR_STR));
   sprintf(ADDR_STR, "%02X%02X%02X%02X%02X%02X%02X%02X", sensorAddr[0], sensorAddr[1], sensorAddr[2], sensorAddr[3], sensorAddr[4], sensorAddr[5], sensorAddr[6], sensorAddr[7]);
   
   tempStartTime = millis();
-  /*
-  if ( u8g.getMode() == U8G_MODE_BW )
+
+  Wire.beginTransmission(address);
+  if (Wire.endTransmission() == 0)
+  {
+    oledPresent = true;
+    debugSerial.println("Oled is present");  
+  }
+
+  if (oledPresent && (u8g.getMode() == U8G_MODE_BW ))
   {
     u8g.setColorIndex(1);
   }
-  */
 }
 
 void loop()
 {
   unsigned long currentTime = millis();
+  
+  if (currentTime < tempStartTime)
+  {//Time machine; overflow of long int
+    tempStartTime = millis();
+    currentTime = millis();
+    tempRequested = false;
+  }
   if (touchUp.capacitiveSensor(30) > 100)
   {
     if (setTempReceived || ((currentTime - setSendStartTime) > 3000)) //If response received, or more than 3 sec ago send
     {
+      debugSerial.println("Up pressed");  
       xbeeSend("[6:%s]", ADDR_STR);
       setSendStartTime = millis();
       setTempReceived = false;
@@ -179,27 +216,35 @@ void loop()
   {
     if (setTempReceived || ((currentTime - setSendStartTime) > 3000)) //If response received, or more than 3 sec ago send
     {
+      debugSerial.println("Down pressed");  
       xbeeSend("[7:%s]", ADDR_STR); 
       setSendStartTime = millis();
       setTempReceived = false;
     }
   }
-  
+  //Request measurement of the temperature
   if (!tempRequested && (((currentTime - tempStartTime) > TEMP_INTERVAL_SECONDS * 1000) || bootFlag))
   {
+    debugSerial.println("Request temperature");  
     tempStartTime = millis();
     sensors.requestTemperatures(); // Send the command to get temperatures
     tempRequested = true;
     bootFlag = false;
   }
+
+  //Read the temperature
   if (tempRequested && ((currentTime - tempStartTime) > TEMP_MEASURE_TIME_SECONDS * 1000))
   {
+    debugSerial.println("Read temperature");  
     tempStartTime = millis();
     tempRequested = false;
-      = sensors.getTempCByIndex(0);    
+    tempCurrent = sensors.getTempCByIndex(0);    
     char tempStr[4];
     dtostrf(tempCurrent, 2, 2, tempStr);
-    
+    debugSerial.print("Temperature:");  
+    debugSerial.println(tempStr);
+
+    //Send the temperature
     //If still using the broadcast address
     if (sensorListener.getMsb() == 0 && sensorListener.getLsb() == 0x0000FFFF)
     {
@@ -222,49 +267,76 @@ void loop()
         xbee.getResponse().getTxStatusResponse(xbTxStatus);
         lastAcknowledged = xbTxStatus.isSuccess();
       }
+     
       if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
       {
         xbee.getResponse().getTxStatusResponse(xbRx);
         if (xbRx.getDataLength() > 4)
         {
           uint8_t* rawData = xbRx.getData();
-          if (rawData[0] == '[' && rawData[xbRx.getDataLength() - 1] == ']')
+          debugSerial.print("Received data 1");  
+          
+          if (xbRx.getDataLength() > 4)
           {
-            //If we received the DigiMesh address of the server
-            if ((rawData[1] == '4') && (xbRx.getDataLength() == 20))
+            if (rawData[0] == '[' && rawData[xbRx.getDataLength() - 1] == ']')
             {
-              char msbStr[10];
-              char lsbStr[10];
-              memset(msbStr, 0, sizeof(msbStr));
-              memset(lsbStr, 0, sizeof(lsbStr));
+              //If we received the DigiMesh address of the server
+              if ((rawData[1] == '4') && (xbRx.getDataLength() == 20))
+              {
+                debugSerial.println("D1");  
+                debugSerial.print(xbRx.getDataLength());  
+                
+                char msbStr[10];
+                char lsbStr[10];
+                //for(int i=0;i<10;i++){msbStr[i] = 0;};
+                //for(int i=0;i<10;i++){lsbStr[i] = 0;};
+                //Memset doesn't work ?
+                memset(msbStr, 0, sizeof(msbStr));
+                memset(lsbStr, 0, sizeof(lsbStr));
+                
+                debugSerial.println("D2");  
+                
+                memcpy((void*)msbStr, &rawData[3], 8);
+                memcpy((void*)lsbStr, &rawData[11], 8);
+                
+                uint32_t msb = strtol(msbStr, NULL, 16);
+                uint32_t lsb = strtol(lsbStr, NULL, 16);
+                debugSerial.print("Received data 4");  
+  
+                sensorListener.setMsb(msb); 
+                sensorListener.setLsb(lsb);
+                debugSerial.print("Received data 5");  
+  
+                xbeeSend("[1:%s]", ADDR_STR);
+                
+              }
               
-              memcpy((void*)msbStr, &rawData[3], 8);
-              memcpy((void*)lsbStr, &rawData[11], 8);
-              uint32_t msb = strtol(msbStr, NULL, 16);
-              uint32_t lsb = strtol(lsbStr, NULL, 16);
-              sensorListener.setMsb(msb); 
-              sensorListener.setLsb(lsb);
-              xbeeSend("[1:%s]", ADDR_STR);
+              //We received the set temperature from the server
+              if (rawData[1] == '5')
+              {
+                memset(SET_TEMPERATURE, 0, sizeof(SET_TEMPERATURE));
+                memcpy(SET_TEMPERATURE, &rawData[3], xbRx.getDataLength() - 4); // len([5:])
+                dispStartTime = millis();
+                displaySetTemperature = true;
+                setTempReceived = true;
+              }
             }
-            //We received the set temperature from the server
-            if (rawData[1] == '5')
-            {
-              memset(SET_TEMPERATURE, 0, sizeof(SET_TEMPERATURE));
-              memcpy(SET_TEMPERATURE, &rawData[3], xbRx.getDataLength() - 4); // len([5:])
-              dispStartTime = millis();
-              displaySetTemperature = true;
-              setTempReceived = true;
-            }              
           }
+            
         }
-
       }        
   }
+
+  if (oledPresent)
+  {
+    u8g.firstPage();  
+    do
+    {
+      draw();
+    } while( u8g.nextPage() );
+  }
+  else
+  {
    delay(100); 
-   /*
-  u8g.firstPage();  
-  do {
-    draw();
-  } while( u8g.nextPage() );
-  */
+  }
 }
