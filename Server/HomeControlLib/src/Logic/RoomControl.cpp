1f5c9cd6d66e086fee8c5c4bf167e335f6e40ca6
Kbij <Kbij@users.noreply.github.com>
2019-01-16 21:58:19 +0100

    set temp

--- Server/HomeControlLib/src/Logic/RoomControl.cpp
+++ Server/HomeControlLib/src/Logic/RoomControl.cpp
@@ -32,7 +32,7 @@ RoomControl::RoomControl(const std::string& roomId, uint8_t heaterOutput, const 
 	mThermostat(thermostat),
 	mHeaterListener(heaterListener),
 	mRoomTemperature(21.0),
-	mSetTemperature(21),
+	mSetTemperature(16),
 	mWorkerThread(nullptr),
 	mWorkerThreadRunning(false),
 	mUpRequested(0),
