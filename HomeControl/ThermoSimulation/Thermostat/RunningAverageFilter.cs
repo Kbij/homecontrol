da6d0b57d956e6993d58a281385fb91679fb5d35
Kbij <Kbij@users.noreply.github.com>
2019-02-13 23:08:02 +0100

    missing

---
+++ HomeControl/ThermoSimulation/Thermostat/RunningAverageFilter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThermoSimulation.Thermostat
+{
+    class RunningAverageFilter
+    {
+        private int mNumberOfPoints;
+        private List<decimal> mSum;
+
+        public RunningAverageFilter(int points)
+        {
+            mNumberOfPoints = points;
+            mSum = new List<decimal>();
+        }
+
+        public bool isStable()
+        {
+            return (mSum.Count >= mNumberOfPoints);
+        }
+
+        public decimal AddValue(decimal value)
+        {
+            mSum.Add(value);
+            while (mSum.Count > mNumberOfPoints)
+            {
+                mSum.RemoveAt(0);
+            }
+            if (mSum.Count == mNumberOfPoints)
+            {
+                return mSum.Sum() / mNumberOfPoints;
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+}
