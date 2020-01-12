cab85e4173554221a720344bcc6e4b2974b0c59c
Kbij <Kbij@users.noreply.github.com>
2020-01-12 18:21:55 +0100

    SendosDisplay class

---
+++ Arduino/SensorIO/src/SensorDisplay.cpp
@@ -0,0 +1,97 @@
+/*
+** EPITECH PROJECT, 2020
+** Sensor
+** File description:
+** SensorDisplay
+*/
+
+#include "SensorDisplay.h"
+#include <Fonts/FreeMonoBold18pt7b.h>
+#include <Fonts/FreeMonoBold24pt7b.h>
+
+namespace
+{
+uint8_t POS_ROOMNAME = 30;
+uint8_t POS_TEMPERATURE = 150;
+}
+
+namespace SensorNs
+{
+SensorDisplay::SensorDisplay():
+    mDisplay(GxEPD2_154_D67(/*CS=10*/ SS, /*DC=*/ 9, /*RST=*/ 8, /*BUSY=*/ 7)),
+    mRoomName(),
+    mNeedsUpdate()
+{
+}
+
+void SensorDisplay::init()
+{
+    mDisplay.init(0);
+    mDisplay.setRotation(1);
+    mDisplay.setTextColor(GxEPD_BLACK);
+    mDisplay.setFullWindow();
+    mDisplay.firstPage();   
+}
+
+void SensorDisplay::roomName(String name)
+{
+    if (mRoomName != name)
+    {
+        mNeedsUpdate = true;
+    }
+    mRoomName = name;
+}
+
+void SensorDisplay::roomTemperature(double temperature)
+{
+    if (abs(mRoomTemperature - temperature) > 0.1)
+    {
+        mNeedsUpdate = true;
+    }
+    mRoomTemperature = temperature;
+}
+
+void SensorDisplay::writeMid(int vert, const char* str)
+{
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  mDisplay.getTextBounds(str, 0, 0, &tbx, &tby, &tbw, &tbh);
+  int y = (mDisplay.width() - tbw)/2;
+  mDisplay.setCursor(y, vert);
+  mDisplay.print(str);
+}
+
+void SensorDisplay::drawRoomName()
+{
+    mDisplay.setFont(&FreeMonoBold18pt7b);
+    writeMid(POS_ROOMNAME, mRoomName.c_str());
+    // mDisplay.setCursor(50, );
+    // mDisplay.print(mRoomName);
+}
+
+void SensorDisplay::drawTemperature()
+{
+    char tmpString[5];
+    dtostrf(mRoomTemperature,4, 1, tmpString);
+    mDisplay.setFont(&FreeMonoBold24pt7b);
+    writeMid(POS_TEMPERATURE, tmpString);
+    // mDisplay.setCursor(50, POS_TEMPERATURE);
+    // String temperature(tmpString);
+    // mDisplay.print(temperature);
+}
+
+void SensorDisplay::draw()
+{
+    if (mNeedsUpdate)
+    {
+        mDisplay.firstPage();  
+        do
+        {
+            drawRoomName();
+            drawTemperature();
+        //draw();
+        } while( mDisplay.nextPage());
+    }
+
+    mNeedsUpdate = false;
+}
+}
---
+++ Arduino/SensorIO/src/SensorDisplay.h
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2020
+** Sensor
+** File description:
+** SensorDisplay
+*/
+
+#ifndef SENSORDISPLAY_H_
+#define SENSORDISPLAY_H_
+#include <Arduino.h>
+#pragma GCC diagnostic push 
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#include <GxEPD2_BW.h>
+
+#pragma GCC diagnostic pop
+
+namespace SensorNs
+{
+
+#define MAX_DISPLAY_BUFFER_SIZE 800
+#define MAX_HEIGHT(EPD) (EPD::HEIGHT <= MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8) ? EPD::HEIGHT : MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8))
+
+class SensorDisplay
+{
+public:
+    SensorDisplay();
+    
+    void init();
+    void roomName(String name);
+    void roomTemperature(double temperature);
+    void draw();
+private:
+    void writeMid(int vert, const char* str);
+    void drawRoomName();
+    void drawTemperature();
+    GxEPD2_BW<GxEPD2_154_D67, MAX_HEIGHT(GxEPD2_154_D67)> mDisplay;
+    String mRoomName;
+    double mRoomTemperature;
+    bool mNeedsUpdate;
+}; 
+}
+#endif /* !SENSORDISPLAY_H_ */
